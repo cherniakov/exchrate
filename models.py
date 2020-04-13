1945ae593c6479a0080b1dec7efaadd1dc481951
Aleksei <chernyakov@yandex.ru>
2020-04-13 15:02:50 +0300

    Создание  файла test_api.py

--- models.py
+++ models.py
@@ -5,12 +5,7 @@ from peewee import SqliteDatabase, Model, IntegerField, DoubleField, DateTimeFie
 db = SqliteDatabase('golden-eye.db')
 
 
-class BaseModel(Model):
-    class Meta:
-        database = db
-
-
-class XRate(BaseModel):
+class XRate(Model):
     """Курсы валют"""
     from_currency = IntegerField()
     to_currency = IntegerField()
@@ -18,7 +13,18 @@ class XRate(BaseModel):
     update = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
+        database = db
         db_table = "xrates"
         indexes = (
             (('from_currency', 'to_currency'), True),
         )
+
+    def __str__(self):
+        return "XRate(%s=>%s): %s" % (self.from_currency, self.to_currency, self.rate)
+
+
+def init_db():
+    db.drop_tables(XRate)
+    XRate.create_table()
+    XRate.create(from_currency=840, to_currency=980, rote=1)
+    print("db_created!")

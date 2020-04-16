519afe67be3386eb2bef0d608797331558264074
Aleksei <chernyakov@yandex.ru>
2020-04-16 19:18:14 +0300

    Создание cbr_api.py и тестов

--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ import unittest
 import test_api
 import models
 import privat_api
+import cbr_api
 
 
 class Test(unittest.TestCase):
@@ -25,6 +26,17 @@ class Test(unittest.TestCase):
         update_after = xrate.updated
 
         self.assertGreater(xrate.rate, 25)
+        self.assertGreater(update_after, update_before)
+
+    def test_cbr(self):
+        xrate = models.XRate.get(from_currency=840, to_currency=643)
+        update_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        cbr_api.updata_xrates(840, 643)
+        xrate = models.XRate.get(from_currency=840, to_currency=643)
+        update_after = xrate.updated
+
+        self.assertGreater(xrate.rate, 60)
         self.assertGreater(update_after, update_before)
 
 

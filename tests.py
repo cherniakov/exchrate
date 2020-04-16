7c67bf89ba54e721c5f6987faa25d50db8a41770
Aleksei <chernyakov@yandex.ru>
2020-04-16 16:23:50 +0300

    Создание privat_api.py и написание тестов к нему

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@ import unittest
 
 import test_api
 import models
+import privat_api
 
 
 class Test(unittest.TestCase):
@@ -14,6 +15,17 @@ class Test(unittest.TestCase):
         test_api.update_xrates(840, 980)
         xrate = models.XRate.get(id=1)
         self.assertEqual(xrate.rate, 1.01)
+
+    def test_privat(self):
+        xrate = models.XRate.get(id=1)
+        update_before = xrate.updated
+        self.assertEqual(xrate.rate, 1.0)
+        privat_api.updata_xrates(840, 980)
+        xrate = models.XRate.get(id=1)
+        update_after = xrate.updated
+
+        self.assertGreater(xrate.rate, 25)
+        self.assertGreater(update_after, update_before)
 
 
 if __name__ == '__main__':

81527d1f0c590b533ebc69d0fb84595101423c7c
Aleksei <chernyakov@yandex.ru>
2020-04-17 11:14:27 +0300

    Рефакторинг кода, создания класса Api изменение теста

---
+++ api/privat_api.py
@@ -0,0 +1,29 @@
+import requests
+from api import _Api
+
+
+class Api(_Api):
+    def __init__(self):
+        super().__init__("PrivatApi")
+
+    def _updata_rate(self, xrate):
+        rate = self._get_privat_rate(xrate.from_currency)
+        return rate
+
+    def _get_privat_rate(self, from_currency):
+        response = requests.get("https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11")
+        response_json = response.json()
+        self.log.debug("Privat response: %s" % response_json)
+        usd_rate = self._find_usd_rate(response_json, from_currency)
+
+        return usd_rate
+
+    def _find_usd_rate(self, response_data, from_currency):
+        pr_valute_map = {840: "USD"}
+        currency_pr_alias = pr_valute_map[from_currency]
+
+        for e in response_data:
+            if e["ccy"] == currency_pr_alias:
+                return float(e["sale"])
+
+        raise ValueError("Invalid Privat response: USD not found ")
--- api/tests.py
+++ api/tests.py
@@ -1,9 +1,9 @@
 import unittest
-
-import test_api
 import models
-import privat_api
-import cbr_api
+
+import api.test_api as test_api
+import api.privat_api as privat_api
+import api.cbr_api as cbr_api
 
 
 class Test(unittest.TestCase):
@@ -12,16 +12,19 @@ class Test(unittest.TestCase):
 
     def test_main(self):
         xrate = models.XRate.get(id=1)
+        updated_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
         test_api.update_xrates(840, 980)
         xrate = models.XRate.get(id=1)
+        updated_after = xrate.updated
         self.assertEqual(xrate.rate, 1.01)
+        self.assertGreater(updated_after, updated_before)
 
     def test_privat(self):
         xrate = models.XRate.get(id=1)
         update_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        privat_api.updata_xrates(840, 980)
+        privat_api.Api().updata_rate(840, 980)
         xrate = models.XRate.get(id=1)
         update_after = xrate.updated
 
@@ -32,10 +35,10 @@ class Test(unittest.TestCase):
         xrate = models.XRate.get(from_currency=840, to_currency=643)
         update_before = xrate.updated
         self.assertEqual(xrate.rate, 1.0)
-        cbr_api.updata_xrates(840, 643)
+        cbr_api.Api().updata_rate(840, 643)
+
         xrate = models.XRate.get(from_currency=840, to_currency=643)
         update_after = xrate.updated
-
         self.assertGreater(xrate.rate, 60)
         self.assertGreater(update_after, update_before)
 

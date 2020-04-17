02435a6a7bcbf8c96e51badaad2ea21cbea5ef02
Aleksei <chernyakov@yandex.ru>
2020-04-17 15:22:39 +0300

    Создание моделей таблиц бд ApiLog ErrorLog

--- models.py
+++ models.py
@@ -1,10 +1,16 @@
-from peewee import SqliteDatabase, Model, IntegerField, DoubleField, DateTimeField, datetime as peewee_datetime
+from peewee import (SqliteDatabase, Model, IntegerField, DoubleField, DateTimeField,
+                    datetime as peewee_datetime, CharField, TextField)
 from config import DB_NAME
 
 db = SqliteDatabase(DB_NAME)
 
 
-class XRate(Model):
+class _Model(Model):
+    class Meta:
+        database = db
+
+
+class XRate(_Model):
     """Курсы валют"""
     from_currency = IntegerField()
     to_currency = IntegerField()
@@ -12,7 +18,6 @@ class XRate(Model):
     updated = DateTimeField(default=peewee_datetime.datetime.now)
 
     class Meta:
-        database = db
         db_table = "xrates"
         indexes = (
             (("from_currency", "to_currency"), True),
@@ -22,9 +27,39 @@ class XRate(Model):
         return "XRate(%s=>%s): %s" % (self.from_currency, self.to_currency, self.rate)
 
 
+class ApiLog(_Model):
+    request_url = CharField()
+    request_data = TextField(null=True)
+    request_method = CharField(max_length=100)
+    request_headers = TextField(null=True)
+    response_text = TextField(null=True)
+    created = DateTimeField(index=True, default=peewee_datetime.datetime.now)
+    finished = DateTimeField()
+    error = TextField(null=True)
+
+    class Meta:
+        db_name = "api_logs"
+
+
+class ErrorLog(_Model):
+
+    request_data = TextField(null=True)
+    request_url = TextField()
+    request_method = CharField(max_length=100)
+    error = TextField(null=True)
+    traceback = TextField(null=True)
+    created = DateTimeField(index=True, default=peewee_datetime.datetime.now)
+
+    class Meta:
+        db_name = "error_logs"
+
+
 def init_db():
-    db.drop_tables(XRate)
-    XRate.create_table()
+    for m in (XRate, ApiLog, ErrorLog):
+        m.drop_table()
+        m.create_table()
+
     XRate.create(from_currency=840, to_currency=980, rate=1)
     XRate.create(from_currency=840, to_currency=643, rate=1)
+
     print("db_created!")

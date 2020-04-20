0a183bd44426c97a4e9263c604765e5fa958efc1
Aleksei <chernyakov@yandex.ru>
2020-04-20 11:17:14 +0300

    Создание исключение

--- controllers.py
+++ controllers.py
@@ -1,7 +1,10 @@
-from flask import render_template
+from flask import render_template, make_response
 from models import XRate
 
 
 def get_all_rates():
-    xrates = XRate.select()
-    return render_template("xrates.html", xrates=xrates)
+    try:
+        xrates = XRate.select()
+        return render_template("xrates.html", xrates=xrates)
+    except Exception as ex:
+        return make_response(str(ex), 500)
